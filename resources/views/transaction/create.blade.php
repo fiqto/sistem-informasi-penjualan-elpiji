d77c6f76e1320e79749518267e16b74ac459ba4f
Fiqri Baihaqi <fiqto14@gmail.com>
2023-07-19 07:50:59 +0700

    hapus batal transaksi

--- resources/views/transaction/create.blade.php
+++ resources/views/transaction/create.blade.php
@@ -71,7 +71,6 @@
               </div>
               <div class="text-left">
                 <button type="submit" class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">Simpan</button>
-                <button type="button" data-modal-hide="add-modal" class="text-blue-500 bg-blue-50 hover:bg-blue-100 hover:text-blue-600 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">Batal</button>
               </div>
             </form>
           </div>
