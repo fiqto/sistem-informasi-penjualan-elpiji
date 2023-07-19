9e48789d92a56c7041d920f4490224b83a562e8a
Fiqri Baihaqi <fiqto14@gmail.com>
2023-07-20 05:51:17 +0700

    add icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link href="images/logo.png" rel="shortcut icon">
+
         {{-- Select2 --}}
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
